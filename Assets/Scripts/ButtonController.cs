623304d5d232d7618af14b85c50d85e5b3a689a0
Seeker495 <yoshimitsu12@outlook.com>
2022-05-28 17:44:05 +0900

    I modified button process.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-
-    public void PressButton()
+    public void OnClick()
     {
         SoundManager soundManager = SoundManager.Instance;
         soundManager.PlaySFX(soundManager.GetSFX("Click"));
+    }
+
+    public void OnHover()
+    {
+        SoundManager soundManager = SoundManager.Instance;
+        soundManager.PlaySFX(soundManager.GetSFX("Hover"));
     }
 }
